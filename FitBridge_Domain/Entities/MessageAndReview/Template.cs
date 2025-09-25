e453fd5d46c78fc163e360cc871c9676aec723a1
quanggiap <nguyenquanggiap642004@gmail.com>
2025-09-25 10:32:37 +0700

    #31 Add TemplateTile property to Template entity

--- FitBridge_Domain/Entities/MessageAndReview/Template.cs
+++ FitBridge_Domain/Entities/MessageAndReview/Template.cs
@@ -7,7 +7,7 @@ public class Template : BaseEntity
 {
     public EnumContentType ContentType { get; set; }
     public TemplateCategory Category { get; set; }
-
+    public string TemplateTile { get; set; }
     public string TemplateBody { get; set; } = null!;
     public Notification InAppNotification { get; set; } = new Notification();
 }
