f9d2130396b199ca587fa836b00e2222f4c008e2
imbatcat <ptdat04@gmail.com>
2025-10-02 13:31:08 +0700

    Update GetAllFreelancePTPackagesDto.cs

--- FitBridge_Application/Dtos/FreelancePTPackages/GetAllFreelancePTPackagesDto.cs
+++ FitBridge_Application/Dtos/FreelancePTPackages/GetAllFreelancePTPackagesDto.cs
@@ -5,5 +5,19 @@
         public Guid Id { get; set; }
 
         public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int DurationInDays { get; set; }
+
+        public int SessionDurationInMinutes { get; set; }
+
+        public int NumOfSessions { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public Guid PtId { get; set; }
     }
 }
