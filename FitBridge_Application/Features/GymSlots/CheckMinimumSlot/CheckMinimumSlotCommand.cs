34974e3814fdc6eafed4d686156803fe1edbfa4e
quanggiap <nguyenquanggiap642004@gmail.com>
2025-10-02 22:33:25 +0700

    #72 Open Add validation and improve docs for gym slot endpoints
    Added XML documentation and response annotations to GymSlotsController endpoints for better API clarity. Introduced [Required] validation attributes to command and parameter DTOs for slot registration, deactivation, and booking. Updated GetCustomerBookingByCustomerIdSpecification to filter bookings by date.

--- FitBridge_Application/Features/GymSlots/CheckMinimumSlot/CheckMinimumSlotCommand.cs
+++ FitBridge_Application/Features/GymSlots/CheckMinimumSlot/CheckMinimumSlotCommand.cs
@@ -1,11 +1,14 @@
 using System;
 using FitBridge_Application.Dtos.GymSlots;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace FitBridge_Application.Features.GymSlots.CheckMinimumSlot;
 
 public class CheckMinimumSlotCommand : IRequest<CheckMinimumSlotResponseDto>
 {
+    [Required]
     public DateOnly StartWeek { get; set; }
+    [Required]
     public DateOnly EndWeek { get; set; }
 }
