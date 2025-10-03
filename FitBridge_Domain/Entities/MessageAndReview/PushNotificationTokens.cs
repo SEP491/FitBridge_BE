697c59d299b423532c9dc4ce72eb20c0eea6c177
quanggiap <nguyenquanggiap642004@gmail.com>
2025-10-04 00:04:38 +0700

    #72 Add platform support to push notification tokens
    Introduced PlatformEnum to represent device platforms (iOS, Android) and updated PushNotificationTokens entity to include a Platform property. Updated the entity configuration to store the platform as a string in the database.

--- FitBridge_Domain/Entities/MessageAndReview/PushNotificationTokens.cs
+++ FitBridge_Domain/Entities/MessageAndReview/PushNotificationTokens.cs
@@ -1,4 +1,5 @@
 using FitBridge_Domain.Entities.Identity;
+using FitBridge_Domain.Enums.Notifications;
 
 namespace FitBridge_Domain.Entities.MessageAndReview;
 
@@ -7,6 +8,7 @@ public class PushNotificationTokens : BaseEntity
     public string DeviceToken { get; set; }
 
     public Guid UserId { get; set; }
+    public PlatformEnum Platform { get; set; } // e.g., iOS, Android, Web
 
     public ApplicationUser User { get; set; }
 }
