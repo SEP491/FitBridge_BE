4aae86f04c6bdf62b15fd40ed0e856c3b81dba53
imbatcat <ptdat04@gmail.com>
2025-10-10 11:51:53 +0700

    #100 Add get coupon by id

---
+++ FitBridge_Application/Features/Coupons/GetCouponById/GetCouponByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FitBridge_Application.Dtos.Coupons;
+using FitBridge_Application.Interfaces.Repositories;
+using FitBridge_Application.Specifications.Coupons.GetCouponById;
+using FitBridge_Domain.Entities.Orders;
+using FitBridge_Domain.Exceptions;
+using MediatR;
+
+namespace FitBridge_Application.Features.Coupons.GetCouponById
+{
+    internal class GetCouponByIdQueryHandler(
+        IUnitOfWork unitOfWork,
+        IMapper mapper) : IRequestHandler<GetCouponByIdQuery, GetCouponsDto>
+    {
+        public async Task<GetCouponsDto> Handle(GetCouponByIdQuery request, CancellationToken cancellationToken)
+        {
+            var spec = new GetCouponByIdSpecification(request.Id, getActiveOnly: true);
+            var couponDto = await unitOfWork.Repository<Coupon>()
+                .GetBySpecificationProjectedAsync<GetCouponsDto>(spec, mapper.ConfigurationProvider)
+                ?? throw new NotFoundException(nameof(Coupon));
+
+            return couponDto;
+        }
+    }
+}
