733112bc3027572e44e310d7ead3d69a029c8b72
imbatcat <ptdat04@gmail.com>
2025-10-12 17:36:36 +0700

    #118 Propagate to redis backplane

--- FitBridge_Infrastructure/Services/Notifications/NotificationsBackgroundService.cs
+++ FitBridge_Infrastructure/Services/Notifications/NotificationsBackgroundService.cs
@@ -41,6 +41,12 @@ namespace FitBridge_Infrastructure.Services.Notifications
                             logger.LogInformation("Template title {Title}; Template body {Body}; User id: {Id}", dto.Title, dto.Body, userId.ToString());
 
                             var connectionIds = await notificationConnectionManager.GetConnectionsAsync(userId.ToString());
+
+                            foreach (var connectionId in connectionIds)
+                            {
+                                logger.LogInformation("Notified connectionId: {Id}", connectionId);
+                            }
+
                             await hubContext.Clients.Clients(connectionIds.ToList()).NotificationReceived();
 
                             var handshakeContext = new HandshakeContext
