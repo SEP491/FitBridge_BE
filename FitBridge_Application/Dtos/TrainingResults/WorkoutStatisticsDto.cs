f220d055506a2a24ebc02f2120df6ce93c5a00e9
imbatcat <ptdat04@gmail.com>
2025-10-16 17:54:54 +0700

    Add planned number of reps & practice time to training result

--- FitBridge_Application/Dtos/TrainingResults/WorkoutStatisticsDto.cs
+++ FitBridge_Application/Dtos/TrainingResults/WorkoutStatisticsDto.cs
@@ -4,6 +4,10 @@ public class WorkoutStatisticsDto
 {
     public double TotalWeightLifted { get; set; }
 
+    public int PlannedNumOfReps { get; set; }
+
+    public double PlannedPracticeTime { get; set; }
+
     public int TotalRepsCompleted { get; set; }
 
     public double TotalPracticeTimeSeconds { get; set; }
