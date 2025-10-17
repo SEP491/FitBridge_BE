c7a5f30d09ac298f13b643e632f1c18b3429f00d
imbatcat <ptdat04@gmail.com>
2025-10-17 22:10:05 +0700

    Update CreateMeetingCommandHandler.cs

--- FitBridge_Application/Features/Meetings/CreateMeeting/CreateMeetingCommandHandler.cs
+++ FitBridge_Application/Features/Meetings/CreateMeeting/CreateMeetingCommandHandler.cs
@@ -2,6 +2,7 @@
 using FitBridge_Application.Interfaces.Repositories;
 using FitBridge_Application.Specifications.Bookings;
 using FitBridge_Application.Specifications.Bookings.GetBookingById;
+using FitBridge_Application.Specifications.Meetings;
 using FitBridge_Domain.Entities.Gyms;
 using FitBridge_Domain.Entities.Meetings;
 using FitBridge_Domain.Entities.Trainings;
@@ -15,18 +16,33 @@ namespace FitBridge_Application.Features.Meetings.CreateMeeting
     {
         async Task<CreateMeetingDto> IRequestHandler<CreateMeetingCommand, CreateMeetingDto>.Handle(CreateMeetingCommand request, CancellationToken cancellationToken)
         {
+            var meetingSpec = new GetMeetingSessionByBookingIdSpecification(request.BookingId);
+            var existingMeeting = await unitOfWork.Repository<MeetingSession>().GetBySpecificationAsync(
+                meetingSpec);
+
+            if (existingMeeting != null)
+            {
+                throw new DuplicateException("Meeting session already exists for this booking.");
+            }
+
             var spec = new GetBookingByIdSpec(request.BookingId);
-            var booking = await unitOfWork.Repository<Booking>().GetBySpecificationAsync(spec)
+            var booking = await unitOfWork.Repository<Booking>().GetBySpecificationAsync(
+                spec,
+                asNoTracking: false)
                     ?? throw new NotFoundException(nameof(Booking));
 
             var newMeetingSession = new MeetingSession
             {
+                Id = Guid.NewGuid(),
                 UserOneId = booking.PtId!.Value,
                 UserTwoId = booking.CustomerId,
                 BookingId = request.BookingId
             };
 
             unitOfWork.Repository<MeetingSession>().Insert(newMeetingSession);
+            booking.MeetingSession = newMeetingSession;
+
+            unitOfWork.Repository<Booking>().Update(booking);
             await unitOfWork.CommitAsync();
 
             return new CreateMeetingDto { Id = newMeetingSession.Id };
